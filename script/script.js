0566c3bb68745f36f5a0de425afa6e5bcda5ed87
Manoel Monteiro <manoel.monteiro1@outlook.com>
2022-11-06 17:03:23 -0300

    1

---
+++ script/script.js
@@ -0,0 +1,17 @@
+var num2 = 0;
+
+function conversor() {
+
+    var num1 = document.getElementById('num1').value;
+    var camp1 = document.getElementById('camp1');
+
+    num1 = parseFloat(reais);
+
+    console.log(num2);
+
+    num2 = num1 * 2.54;
+
+    num2 = parseFloat(camp1);
+
+    camp1 = document.write(num2);
+}
