e42482882b825d3d2eb591670e6d0146b3bf8b89
Manoel Monteiro <manoel.monteiro1@outlook.com>
2022-11-12 20:09:15 -0300

    created project-base

--- script/script.js
+++ script/script.js
@@ -1,17 +1,10 @@
-var num2 = 0;
+function lenghtConverter(valNum) {
+    document.getElementById("outputInches").innerHTML = valNum / 2.54 || valNum * 0.39370;
+    document.getElementById("outputMeters").innerHTML = valNum / 100;
+    document.getElementsByClassName('class-outputMeters').innerHTML = valNum / 100;
+}
 
-function conversor() {
-
-    var num1 = document.getElementById('num1').value;
-    var camp1 = document.getElementById('camp1');
-
-    num1 = parseFloat(reais);
-
-    console.log(num2);
-
-    num2 = num1 * 2.54;
-
-    num2 = parseFloat(camp1);
-
-    camp1 = document.write(num2);
+function changeFont() {
+    var fon = document.getElementById("main-body");
+    if 
 }
