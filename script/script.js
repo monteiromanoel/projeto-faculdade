dddd3628b5917e896d8d588878a3c2e2a8d58c1b
Manoel Monteiro <manoel.monteiro1@outlook.com>
2022-11-13 20:22:32 -0300

    updates again

--- script/script.js
+++ script/script.js
@@ -1,7 +1,12 @@
-function lenghtConverter(valNum) {
-    document.getElementById("outputInches").innerHTML = valNum / 2.54 || valNum * 0.39370;
+function lenghtConverter(valNum) { 
     document.getElementById("outputMeters").innerHTML = valNum / 100;
-    document.getElementsByClassName('class-outputMeters').innerHTML = valNum / 100;
+    document.getElementById("outputKm").innerHTML = valNum / 100000;
+    document.getElementById("outputInches").innerHTML = valNum / 2.54 || valNum * 0.39370;
+    document.getElementById("outputYard").innerHTML = valNum / 91.44 || valNum * 0.010936;
+    document.getElementById("outputMile").innerHTML = valNum * 0.0000062137 || valNum / 160900;
+    document.getElementById("outputFoot").innerHTML = valNum * 0.032808 || valNum / 30.48; 
+    //document.getElementsByClassName('class-outputMeters').innerHTML = valNum / 100;
+    
 }
 
 function changeFont() {
