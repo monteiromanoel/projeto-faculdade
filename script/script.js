08bd1755982eced92348a864520ab97ef0db2dd5
Manoel Monteiro <manoel.monteiro1@outlook.com>
2022-11-13 21:44:04 -0300

    update from <p> to grid-layout

--- script/script.js
+++ script/script.js
@@ -1,12 +1,13 @@
 function lenghtConverter(valNum) { 
     document.getElementById("outputMeters").innerHTML = valNum / 100;
-    document.getElementById("outputKm").innerHTML = valNum / 100000;
-    document.getElementById("outputInches").innerHTML = valNum / 2.54 || valNum * 0.39370;
-    document.getElementById("outputYard").innerHTML = valNum / 91.44 || valNum * 0.010936;
+    document.getElementById("outputKm").innerHTML = parseFloat(valNum / 100000).toFixed(4);
+    document.getElementById("outputInches").innerHTML = parseFloat(valNum / 2.54).toFixed(2) || parseFloat(valNum * 0.39370).toFixed(2);
+    document.getElementById("outputYard").innerHTML = parseFloat(valNum / 91.44).toFixed(2) || parseFloat(valNum * 0.010936).toFixed(2);
     document.getElementById("outputMile").innerHTML = valNum * 0.0000062137 || valNum / 160900;
     document.getElementById("outputFoot").innerHTML = valNum * 0.032808 || valNum / 30.48; 
     //document.getElementsByClassName('class-outputMeters').innerHTML = valNum / 100;
-    
+
+    document.getElementById("outputMeters").innerHTML = parseFloat(valNum / 100).toFixed(2);
 }
 
 function changeFont() {
